979f322415cfb4077b10b85d5a6c9dfc68159fd6
sameermd16 <sameerthemc@gmail.com>
2023-08-16 15:55:25 +0530

    added padding to name & para

--- src/Tour.jsx
+++ src/Tour.jsx
@@ -8,17 +8,17 @@ export default function Tour({id, image, name, price, info, removeTour}) {
     return (
         <article className='border border-secondary mb-2 rounded'>
             <img src={image} width="100%" />
-            <h3 className='d-flex justify-content-between py-3'>{name} <span>${price}</span></h3>
-            <p>
+            <h3 className='d-flex justify-content-between py-3 px-2'>{name} <span>${price}</span></h3>
+            <p className='px-2'>
                 {fullInfo ? info : `${info.substring(0, 150)}...`}
                 <button className='btn btn-sm btn-outline-secondary ms-2'
                     onClick={() => setFullInfo(!fullInfo)}
                 >
-                    {fullInfo ? 'show less' : 'read more'}
+                    {fullInfo ? 'Show less' : 'Read more'}
                 </button>
             </p>
             <div className='text-center mb-4'>
-                <button className='btn btn-sm btn-outline-danger' onClick={() => removeTour(id)}>not interested</button>
+                <button className='btn btn-sm btn-outline-danger' onClick={() => removeTour(id)}>Not interested</button>
             </div>
         </article>
     )
